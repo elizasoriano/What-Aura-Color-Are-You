3c40770b1040d3a1f2f07e7cd6f5db981b6e975c
ELIZASORIANO <11617585-ELIZASORIANO@users.noreply.replit.com>
2023-05-31 13:32:20 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let questions = document.querySelector(".questions");
+let result = document.querySelector(".result");
+let button = document.querySelector(".button");
+
+button.onclick = function(){
+  title.style.display = "none";
+  qOne.style.display = "block";
+};
+
+
